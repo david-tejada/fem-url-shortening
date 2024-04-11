2d9a1943181c6a514e5b358f5d3b823ece4a5649
David Tejada <davidxtejada@gmail.com>
2024-04-11 16:33:13 +0200

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(180deg 66% 49%)",
+        red: "hsl(0deg 87% 67%)",
+        violet: { 800: "hsl(257deg 26% 33%)", 900: "hsl(260deg 27% 26%)" },
+        gray: { 100: "hsl(225deg 32% 95%)", 400: "hsl(257deg 8% 63%)" },
+      },
+    },
   },
   plugins: [],
 };
